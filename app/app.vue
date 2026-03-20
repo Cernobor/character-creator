9309f795bd585098c20353b4668af479d6825782
pulcovamon <pulcovamon@gmail.com>
2026-03-20 01:44:10 +0100

    feat: setup pwa

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtPage />
+    <NuxtPwaManifest />
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   future: {
     compatibilityVersion: 4,
   },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Character Creator',
+      short_name: 'Char Creator',
+      theme_color: '#ffffff',
+      start_url: '/',
+      display: 'standalone',
+    },
+    workbox: {
+      navigateFallback: '/',
+            globDirectory: '.output/public',
+            globPatterns: ['**/*.{js,css,html,png,svg,ico,jpg,jpeg,webp,json}'],
+            maximumFileSizeToCacheInBytes: 5000000, // 5MB
+    },
+    devOptions: {
+      enabled: true,
+      type: 'classic',
+    }
+  },
 })
